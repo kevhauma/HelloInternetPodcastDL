b844982a763e5774a709784cec058bdc6969025a
kevhauma <hauman.kevin@hotmail.com>
2018-05-01 18:11:06 +0200

    first init

---
+++ readme.md
@@ -0,0 +1,9 @@
+1. install nodeJS 
+
+2. run install.bat
+
+3. run start.bat
+
+4. find episodes in HI folder
+
+5. download remaining episodes manually from hellinternet.fm
---
+++ script.js
@@ -0,0 +1,36 @@
+let fs = require('fs')
+let request = require('request');
+let axios = require('axios')
+
+let err = []
+
+DL(0)
+
+function DL(i) {
+    axios.get('http://traffic.libsyn.com/hellointernet/' + i + '.mp3')
+        .then(function (res) {
+            request
+                .get(res.request.res.responseUrl)
+                .on('error', function (err) {
+                    console.log("ERROR: episode " + i)
+                    err.push(i)
+                    next(i)
+                })
+                .pipe(fs.createWriteStream('HI/HI' + i + '.mp3'))
+                .on('finish', () => {
+                    console.log("succesfully downloaded episode " + i)
+                    next(i)
+                })
+        }).catch(err => {
+            console.log("ERROR: episode " + i)
+            err.push(i)
+            next(i)
+        })
+}
+
+function next(p) {
+    p++
+    if (p <= 101)
+        DL(p)
+    else console.log("please manually download episodes: " + err.join(", "))
+}
